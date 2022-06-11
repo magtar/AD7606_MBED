3e8985ed5b0f190efbfc0838fd4454d71d069398
magtar <mechatron2000@gmail.com>
2022-06-11 13:58:31 -0600

    added thread Readout working

--- src/main.cpp
+++ src/main.cpp
@@ -5,28 +5,49 @@
 using namespace mbed;
 using namespace events;
 using namespace rtos;
+using namespace std::literals::chrono_literals;
 
 SPI fff(PC_3,PC_2,PI_1,PI_0);
 int16_t raw [8];
 DigitalOut CONVST(PC_15);
+Thread thread(osPriorityRealtime);
+
 
 
 void getADC(){
-
+   while(1){
+      //ThisThread::sleep_for(std::chrono::milliseconds(100) ); 
+      //ThisThread::sleep_for(500us); 
+      wait_us(1);
+      CONVST=1;
+      wait_us(3);
+      for(int ii=0;ii<8;ii++){
+         raw[ii]=fff.write((int) 0x00);
+         //Serial.print(raw[ii]);
+         //Serial.print(",");
+      }   
+      //Serial.print(" \n");
+      CONVST=!CONVST;
+   }
 }
 
 int data;
 void setup() {
    Serial.begin(9600);  
-   fff.frequency(20000000);
+   fff.frequency(36000000);
    fff.format(16,0);
    CONVST=1;
+   //osPriority DC=realtime;
+   //thread.set_priority(osPriorityHigh);
+   thread.start(getADC);
    
 }
 
-void loop() {
-   ThisThread::sleep_for(std::chrono::milliseconds(100) ); 
-   CONVST=1;
+void loop() 
+{
+   ThisThread::sleep_for(std::chrono::hours(1) ); 
+   //ThisThread::sleep_for(forever ); 
+  /* CONVST=1;
    wait_us(5);
    for(int ii=0;ii<8;ii++){
       raw[ii]=fff.write((int) 0x00);
@@ -35,4 +56,5 @@ void loop() {
    }   
    //Serial.print(" \n");
    CONVST=!CONVST;
-}
+   */
+} 
