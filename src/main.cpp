fc46c67b12cfc964e159a3fcb78c1106486ad8e8
magtar <mechatron2000@gmail.com>
2022-06-18 17:33:18 -0600

    min

--- src/main.cpp
+++ src/main.cpp
@@ -1,7 +1,5 @@
-#include <mbed.h>
-#include <rtos.h>
-#include <Arduino.h>
 
+#include "mbed/mbed.h"
 using namespace mbed;
 using namespace events;
 using namespace rtos;
@@ -14,38 +12,31 @@ uint8_t j;
 DigitalOut CONVST(PC_15);
 Thread thread(osPriorityRealtime);
 
-
+Ticker t;
 
 void getADC(){
-      //Serial.begin(9600);
-       while(1){
-     
-      //ThisThread::sleep_for(std::chrono::milliseconds(100) ); 
-      //ThisThread::sleep_for(500us); 
+      while(1){
       wait_us(1);
       CONVST=1;
       wait_us(3);
-      //j=0;
       for(int ii=0;ii<8;ii++){
-         raw [ii]=fff.write((int) 0x00);
-        
+         raw [ii]=fff.write((int) 0x00);        
       }        
       CONVST=!CONVST;
    }
 }
 
-
-void setup() {
-   Serial.begin(9600);   
+int main(){  
+   
    fff.frequency(25600000);
-   fff.format(16,0);
+   fff.format(16,0);  
    CONVST=1;   
    thread.start(getADC); 
-   
-}
-
-void loop() 
-{
-   ThisThread::sleep_for(std::chrono::seconds(1) ); 
-   
+  /*t.attach(&getADC,1.0); 
+  while (1) {
+        
+        ThisThread::sleep_for(200.0);
+    }
+*/
+return 0;
 }
