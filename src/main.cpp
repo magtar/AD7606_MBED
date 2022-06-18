a98ad484f0a8bc08d689ae15957a28fab681438e
magtar <mechatron2000@gmail.com>
2022-06-18 15:40:03 -0600

    working SPI read and parse on data

--- src/main.cpp
+++ src/main.cpp
@@ -9,52 +9,43 @@ using namespace std::literals::chrono_literals;
 
 SPI fff(PC_3,PC_2,PI_1,PI_0);
 int16_t raw [8];
+int16_t data[8];
+uint8_t j;
 DigitalOut CONVST(PC_15);
 Thread thread(osPriorityRealtime);
 
 
 
 void getADC(){
-   while(1){
+      //Serial.begin(9600);
+       while(1){
+     
       //ThisThread::sleep_for(std::chrono::milliseconds(100) ); 
       //ThisThread::sleep_for(500us); 
       wait_us(1);
       CONVST=1;
       wait_us(3);
+      //j=0;
       for(int ii=0;ii<8;ii++){
-         raw[ii]=fff.write((int) 0x00);
-         //Serial.print(raw[ii]);
-         //Serial.print(",");
-      }   
-      //Serial.print(" \n");
+         raw [ii]=fff.write((int) 0x00);
+        
+      }        
       CONVST=!CONVST;
    }
 }
 
-int data;
+
 void setup() {
-   Serial.begin(9600);  
-   fff.frequency(36000000);
+   Serial.begin(9600);   
+   fff.frequency(25600000);
    fff.format(16,0);
-   CONVST=1;
-   //osPriority DC=realtime;
-   //thread.set_priority(osPriorityHigh);
-   thread.start(getADC);
+   CONVST=1;   
+   thread.start(getADC); 
    
 }
 
 void loop() 
 {
-   ThisThread::sleep_for(std::chrono::hours(1) ); 
-   //ThisThread::sleep_for(forever ); 
-  /* CONVST=1;
-   wait_us(5);
-   for(int ii=0;ii<8;ii++){
-      raw[ii]=fff.write((int) 0x00);
-      //Serial.print(raw[ii]);
-      //Serial.print(",");
-   }   
-   //Serial.print(" \n");
-   CONVST=!CONVST;
-   */
-} 
+   ThisThread::sleep_for(std::chrono::seconds(1) ); 
+   
+}
