23df697b34e3cab96618644c19603ad21a80b588
magtar <mechatron2000@gmail.com>
2022-07-17 16:32:04 -0600

    semaphore

--- src/main.cpp
+++ src/main.cpp
@@ -6,31 +6,48 @@ using namespace events;
 using namespace rtos;
 using namespace std::literals::chrono_literals;
 
-//EventQueue queue(32 * EVENTS_EVENT_SIZE);
-EventQueue queue;
+
 DigitalOut CONVST(PC_15);
 int16_t raw [8];  
 SPI fff(PC_3,PC_2,PI_1,PI_0);
+Thread thread(osPriorityRealtime);
+Semaphore ticksema(0);
+void getADC();
+void isrTick();
 
-void getADC(){        
-      CONVST=1;
-      wait_us(3);
-      for(int ii=0;ii<8;ii++){
-         raw [ii]=fff.write((int) 0x00);            
-      CONVST=!CONVST;
-      }
-}
 
-int main(){   
-   fff.frequency(25600000);
-   fff.format(16,0);    
-   Thread thread(osPriorityRealtime);
-   thread.start(callback(&queue, &EventQueue::dispatch_forever));
+int main()
+{   
+   fff.frequency(56000000);
+   fff.format(16,0);      
+   thread.start(getADC);
+
    Ticker t;  
-   t.attach(queue.event(&getADC),1000us);
+   t.attach(isrTick,30us);
+   
    while(1)
    {
       ThisThread::sleep_for(std::chrono::hours::max()); 
    }
    
+}
+
+void isrTick()
+{
+   ticksema.release();
+}
+
+void getADC()
+{          
+   while(1){
+   ticksema.acquire();
+   CONVST=1;
+   wait_us(3);
+  
+   for(int ii=0;ii<4;ii++)
+   {
+    raw [ii]=fff.write((int) 0x00);            
+   }
+   CONVST=!CONVST; 
+   }     
 }
